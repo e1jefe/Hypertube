13b3d102c2c3ebb6e62247f8700d4ed7dc7def2f
Serhii KOROTKOV <skorotko@e1r4p3.unit.ua>
2018-09-28 14:37:48 +0300

    created new brench Cabinet

---
+++ App/app/Http/Controllers/API/PassportController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+use Validator;
+
+class PassportController extends Controller
+{
+    public $successStatus = 200;
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:7',
+            'c_password' => 'required|string|same:password',
+        ]);
+        if ($validator->fails()) {
+            return response()->json(['error'=>$validator->errors()], 401);
+        }
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+        $success['token'] =  $user->createToken('MyApp')->accessToken;
+        $success['name'] =  $user->name;
+        return response()->json(['success'=>$success], $this->successStatus);
+    }
+
+    public function login(){
+        if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){
+            $user = Auth::user();
+//            $success['user'] = $user->id;
+            $success['token'] =  $user->createToken('MyApp')->accessToken;
+            return response()->json(['success' => $success], $this->successStatus);
+        }
+        else{
+            return response()->json(['error'=>'Unauthorised'], 401);
+        }
+    }
+
+    public function getDetails()
+    {
+        $user = Auth::user();
+        return response()->json(['success' => $user], $this->successStatus);
+    }
+
+}
